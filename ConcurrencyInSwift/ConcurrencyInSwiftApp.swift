fddc2e6c7e1ea1a2633d9385ee30ac681858e4c5
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2025-05-11 15:36:05 +0700

    Initial Commit

---
+++ ConcurrencyInSwift/ConcurrencyInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyInSwiftApp.swift
+//  ConcurrencyInSwift
+//
+//  Created by Bao Hoang on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
